b919a9e248b5427aa7fc67c3b10aafb0e1777f49
Darshan Patel <darshan.patel@quarkssystems.com>
2025-01-28 14:41:50 +0530

    Revert "Trade India API updated"
    This reverts commit 50025a7524c3ed6c603216bbdb033d9918412702.

--- trade_india/hooks.py
+++ trade_india/hooks.py
@@ -11,16 +11,12 @@ app_license = "MIT"
 scheduler_events = {
     "daily": [
         "trade_india.trade_india.api.fetch_trade_india_leads"
-    ],
-    "hourly": [
-        "trade_india.trade_india.api.sync_lead_status"
     ]
 }
 
 # Document Events
 doc_events = {
     "Lead": {
-        "after_insert": "trade_india.trade_india.api.process_lead",
-        "on_update": "trade_india.trade_india.api.update_lead"
+        "after_insert": "trade_india.trade_india.api.process_lead"
     }
 }
